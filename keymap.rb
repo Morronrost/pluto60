bb2b7d9cdf614a1ff53b3ff45dc7d2a03f604fa8
Sixty60 <tishel19frenil@innovationsgymnasiet.se>
2022-08-29 03:33:13 +0200

    Corrected MCU placement & tidied routing

--- keymap.rb
+++ keymap.rb
@@ -37,7 +37,7 @@ kbd.add_layer :layer1, %i(
   KC_TAB  KC_Q    KC_UP   KC_E      KC_R    KC_T        KC_Y    KC_U    KC_I     KC_O    KC_P        KC_LBRC
   KC_CAPS KC_LEFT KC_DOWN KC_RIGHT  KC_F    KC_G        KC_H    KC_J    KC_K     KC_L    KC_SCOLON   KC_QUOT
   KC_LSFT KC_Z    KC_X    KC_C      KC_V    KC_B        KC_N    KC_M    KC_COMM  KC_DOT  KC_SLSH     KC_RSFT
-  KC_LCTL KC_LGUI KC_LALT KC_SPC    KC_RALT LSS_LAYER1  KC_RCTL KC_EQL  KC_BSPC  KC_ENT  KC_RBRC     KC_NONUS_HASH
+  KC_LCTL KC_LGUI KC_LALT KC_SPC    KC_RALT LSS_LAYER1  KC_RCTL KC_EQL  KC_DEL   KC_ENT  KC_RBRC     KC_NONUS_HASH
 )
 
 kbd.define_mode_key :LSS_LAYER1,   [ :KC_NONUS_BSLASH,   :layer1, 200, 200]
